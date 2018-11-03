3f0e99d5f0de27d59b2e6c427b2f541ac18bfe71
georkap <gkapidis1@gmail.com>
2018-11-03 21:22:45 +0100

    working detect_video.py

--- detect_video.py
+++ detect_video.py
@@ -6,31 +6,28 @@ script to run Yolov3 on a video and save the video with overlaid detections and 
 
 @author: GEO
 """
-
-from __future__ import division
-
-from models import *
-from utils.utils import *
-from utils.datasets import *
-
 import os
-import sys
-import time
-import datetime
 import argparse
+import time
+import numpy as np
 
 import torch
-from torch.utils.data import DataLoader
-from torchvision import datasets
-from torch.autograd import Variable
+import cv2
 
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-from matplotlib.ticker import NullLocator
+from models import Darknet
+from utils.utils import load_classes, non_max_suppression
+
+def prepare_video_writer(vid_path, size, fps=30):
+    fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
+    vid_dir = os.path.dirname(vid_path)
+    if not os.path.exists(vid_dir):
+        os.makedirs(vid_dir)
+    writer = cv2.VideoWriter(vid_path, fourcc, fps, size)
+    return writer
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--video_file', type=str, default='data/samples', help='path to video')
+    parser.add_argument('--video_file', type=str, help='path to video')
     parser.add_argument('--config_path', type=str, default='config/yolov3.cfg', help='path to model config file')
     parser.add_argument('--weights_path', type=str, default='weights/yolov3.weights', help='path to weights file')
     parser.add_argument('--class_path', type=str, default='data/coco.names', help='path to class label file')
@@ -39,14 +36,12 @@ def main():
     parser.add_argument('--batch_size', type=int, default=1, help='size of the batches')
     parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
     parser.add_argument('--img_size', type=int, default=416, help='size of each image dimension')
+    parser.add_argument('--resize_input', default=False, action='store_true')
     parser.add_argument('--use_cuda', type=bool, default=True, help='whether to use cuda if available')
     opt = parser.parse_args()
     print(opt)
     
     cuda = torch.cuda.is_available() and opt.use_cuda
-    
-    os.makedirs('output', exist_ok=True)
-    
     # Set up model
     model = Darknet(opt.config_path, img_size=opt.img_size)
     model.load_weights(opt.weights_path)
@@ -56,93 +51,78 @@ def main():
     
     model.eval() # Set in evaluation mode
     
-    dataloader = DataLoader(ImageFolder(opt.image_folder, img_size=opt.img_size),
-                            batch_size=opt.batch_size, shuffle=False, num_workers=opt.n_cpu)
-    
     classes = load_classes(opt.class_path) # Extracts class labels from file
-    
-    Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
-    
-    imgs = []           # Stores image paths
-    img_detections = [] # Stores detections for each image index
-    
+        
     print ('\nPerforming object detection:')
-    prev_time = time.time()
-    for batch_i, (img_paths, input_imgs) in enumerate(dataloader):
-        # Configure input
-        input_imgs = Variable(input_imgs.type(Tensor))
+#    prev_time = time.time()
     
-        # Get detections
-        with torch.no_grad():
-            detections = model(input_imgs)
-            detections = non_max_suppression(detections, 80, opt.conf_thres, opt.nms_thres)
+    cap = cv2.VideoCapture(opt.video_file)
+    video_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    video_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    output_vid_path = os.path.join(r"outputs", 
+                                   os.path.basename(opt.video_file).split('.')[0]+"_detections.mp4")
     
+    writer = prepare_video_writer(output_vid_path, (video_width, video_height), fps)
+    print("Saving output video at {}".format(output_vid_path))
+    cap.set(cv2.CAP_PROP_POS_FRAMES, 550)
+
+    resize_input=opt.resize_input
+    # Get detections
+    with torch.no_grad():
+        
+        frame_iter = 0
+        while cap.isOpened():
+            start_time = time.time()
+            ret, frame = cap.read()
+            if ret:
+                # turn ret into input
+                img = frame / 255.
+                if resize_input:
+                    img = cv2.resize(img, (opt.img_size, opt.img_size))
+                    resize_ratio = (video_height/opt.img_size, video_width/opt.img_size)
+                img = np.transpose(img, (2, 0, 1))
+                # As pytorch tensor
+                img = torch.from_numpy(img).float().cuda()
+                img.unsqueeze_(0)
+                detections = model(img)
+                detections = non_max_suppression(detections, 80, opt.conf_thres, opt.nms_thres)
+                # overlay detections on frame and write to video writer
+                out_img = frame.copy()
+#                out_img = cv2.resize(frame/255., (opt.img_size, opt.img_size)).copy()
+                # Draw bounding boxes and labels of detections
+                if detections[0] is not None:
+                    for x1, y1, x2, y2, conf, cls_conf, cls_pred in detections[0]: # get the first image of the batch, works because batch=1
+                        if classes[int(cls_pred)] != 'person': # keep only person detections
+                            continue
+                        if cls_conf < 0.25:
+                            continue
+                        if resize_input:
+                            y1 *= resize_ratio[0]
+                            y2 *= resize_ratio[0]
+                            x1 *= resize_ratio[1]
+                            x2 *= resize_ratio[1]
+                        box_h = y2-y1
+                        box_w = x2-x1
     
-        # Log progress
-        current_time = time.time()
-        inference_time = datetime.timedelta(seconds=current_time - prev_time)
-        prev_time = current_time
-        print ('\t+ Batch %d, Inference Time: %s' % (batch_i, inference_time))
-    
-        # Save image and detections
-        imgs.extend(img_paths)
-        img_detections.extend(detections)
-    
-    # Bounding-box colors
-    cmap = plt.get_cmap('tab20b')
-    colors = [cmap(i) for i in np.linspace(0, 1, 20)]
-    
-    print ('\nSaving images:')
-    # Iterate through images and save plot of detections
-    for img_i, (path, detections) in enumerate(zip(imgs, img_detections)):
-    
-        print ("(%d) Image: '%s'" % (img_i, path))
-    
-        # Create plot
-        img = np.array(Image.open(path))
-        plt.figure()
-        fig, ax = plt.subplots(1)
-        ax.imshow(img)
-    
-        # The amount of padding that was added
-        pad_x = max(img.shape[0] - img.shape[1], 0) * (opt.img_size / max(img.shape))
-        pad_y = max(img.shape[1] - img.shape[0], 0) * (opt.img_size / max(img.shape))
-        # Image height and width after padding is removed
-        unpad_h = opt.img_size - pad_y
-        unpad_w = opt.img_size - pad_x
-    
-        # Draw bounding boxes and labels of detections
-        if detections is not None:
-            unique_labels = detections[:, -1].cpu().unique()
-            n_cls_preds = len(unique_labels)
-            bbox_colors = random.sample(colors, n_cls_preds)
-            for x1, y1, x2, y2, conf, cls_conf, cls_pred in detections:
-    
-                print ('\t+ Label: %s, Conf: %.5f' % (classes[int(cls_pred)], cls_conf.item()))
-    
-                # Rescale coordinates to original dimensions
-                box_h = ((y2 - y1) / unpad_h) * img.shape[0]
-                box_w = ((x2 - x1) / unpad_w) * img.shape[1]
-                y1 = ((y1 - pad_y // 2) / unpad_h) * img.shape[0]
-                x1 = ((x1 - pad_x // 2) / unpad_w) * img.shape[1]
-    
-                color = bbox_colors[int(np.where(unique_labels == int(cls_pred))[0])]
-                # Create a Rectangle patch
-                bbox = patches.Rectangle((x1, y1), box_w, box_h, linewidth=2,
-                                        edgecolor=color,
-                                        facecolor='none')
-                # Add the bbox to the plot
-                ax.add_patch(bbox)
-                # Add label
-                plt.text(x1, y1, s=classes[int(cls_pred)], color='white', verticalalignment='top',
-                        bbox={'color': color, 'pad': 0})
-    
-        # Save generated image with detections
-        plt.axis('off')
-        plt.gca().xaxis.set_major_locator(NullLocator())
-        plt.gca().yaxis.set_major_locator(NullLocator())
-        plt.savefig('output/%d.png' % (img_i), bbox_inches='tight', pad_inches=0.0)
-        plt.close()
+                        cv2.rectangle(out_img, (x1, y1), (x1 + box_w, y1 + box_h), (0,0,255), 3)
+                        cv2.putText(out_img, str(classes[int(cls_pred)]), (x1 + 10, y1 + box_h), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (255,255,255),1)
+#                    cv2.imshow("1",out_img)
+#                    cv2.waitKey(0)
+                writer.write(out_img)
+                # TODO: save detections to txt
+                frame_iter += 1
+                time_for_frame = time.time() - start_time
+                print("\rCompleted:[{}/{}] with {:.3f} fps".format(frame_iter, num_frames, 1./time_for_frame),end="")
+            else:
+                cap.release()
+                break
+    try:
+        writer.release()
+    except:
+        print("didn't release")
+
 
 if __name__=='__main__':
     main()
