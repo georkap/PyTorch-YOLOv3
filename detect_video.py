e6ddc203f4546ee3ee42eca480356f1b90fe8c2a
georkap <gkapidis1@gmail.com>
2018-11-04 10:55:12 +0100

    added class confidence arg

--- detect_video.py
+++ detect_video.py
@@ -31,7 +31,7 @@ def main():
     parser.add_argument('--config_path', type=str, default='config/yolov3.cfg', help='path to model config file')
     parser.add_argument('--weights_path', type=str, default='weights/yolov3.weights', help='path to weights file')
     parser.add_argument('--class_path', type=str, default='data/coco.names', help='path to class label file')
-    parser.add_argument('--class_thresh', type=str, default=0.25)
+    parser.add_argument('--class_thresh', type=float, default=0.25)
     parser.add_argument('--conf_thres', type=float, default=0.8, help='object confidence threshold')
     parser.add_argument('--nms_thres', type=float, default=0.4, help='iou thresshold for non-maximum suppression')
     parser.add_argument('--batch_size', type=int, default=1, help='size of the batches')
